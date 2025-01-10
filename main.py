d50a9597e3a987f8360b77d1280a45f14f27f970
Aswathy <aswathyj118@gmail.com>
2025-01-10 16:12:35 +0530

    commit from second-branch

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Hello world! I am Aswathy!")
+<<<<<<< Updated upstream
 print("12345678910")
 print("Hiiiiiiiii")
+=======
+print("How are you?")
+print("I am fine! Thank you!")
+print("Okayy")
+>>>>>>> Stashed changes
